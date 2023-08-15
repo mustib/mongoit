7b0e387c07a7515fe282ca9f01c17e9bdf31235c
Muhammad Mustafa <mmi197here@gmail.com>
2023-08-15 05:05:07 +0300

    fix: restore toPage method deleted by mistake in MongoDbFind
    - Deleted by mistake while implementing FindOne

--- src/utils/db/MongoDb/MongoDbFind/MongoDbFind.ts
+++ src/utils/db/MongoDb/MongoDbFind/MongoDbFind.ts
@@ -25,8 +25,16 @@ class MongoDbFind<
       const page = this.paginationObject.pageNumber as number;
       const results = this.paginationObject.resultsPerPage as number;
       const skip = (page - 1) * results;
+      cursor.limit(results);
       cursor.skip(skip);
     }
+  }
+
+  toPage(pageNumber = 1, resultsPerPage = 20) {
+    this.hasPaginated = true;
+    this.paginationObject.pageNumber = pageNumber;
+    this.paginationObject.resultsPerPage = resultsPerPage;
+    return this;
   }
 
   // TODO: Implement cursor paradigms. REFERENCE https://www.mongodb.com/docs/drivers/node/current/fundamentals/crud/read-operations/cursor/
