2b09d1d05bc593ea1c53c7e56ad94f409dfa3446
Muhammad Mustafa <mmi197here@gmail.com>
2023-08-25 22:48:43 +0300

    feat: implement deleteProduct from MongoDb

--- src/api/product/controller/product.controller.deleteProduct.ts
+++ src/api/product/controller/product.controller.deleteProduct.ts
@@ -1,9 +1,21 @@
-import { ApiSuccessResponse, catchAsyncRouteHandler } from '../../../utils';
+import {
+  ApiFailResponse,
+  ApiSuccessResponse,
+  catchAsyncRouteHandler,
+} from '../../../utils';
+import productModel from '../product.model';
 
 const deleteProduct = catchAsyncRouteHandler(async (req, res) => {
   const { id } = req.params;
 
-  new ApiSuccessResponse(res).deleted();
+  const { deletedCount } = await productModel.deleteById(id);
+
+  if (deletedCount > 0) {
+    new ApiSuccessResponse(res).deleted() as never;
+    return;
+  }
+
+  new ApiFailResponse(res).setMessage('Product has not found').notFound();
 });
 
 export default deleteProduct;
