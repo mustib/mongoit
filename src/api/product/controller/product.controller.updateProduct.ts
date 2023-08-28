b7422355dd8be4d1d341f2768bc577d52eaa4858
Muhammad Mustafa <mmi197here@gmail.com>
2023-08-28 05:25:15 +0300

    feat: implement updateProduct in MongoDb

--- src/api/product/controller/product.controller.updateProduct.ts
+++ src/api/product/controller/product.controller.updateProduct.ts
@@ -1,11 +1,38 @@
-import { ApiSuccessResponse, catchAsyncRouteHandler } from '../../../utils';
+import {
+  ApiFailResponse,
+  ApiSuccessResponse,
+  catchAsyncRouteHandler,
+} from '../../../utils';
+import productModel from '../product.model';
 
-const updateProduct = catchAsyncRouteHandler(async (req, res) => {
-  const { id } = req.params;
+import type { ProductSchema } from '../product.types';
 
-  new ApiSuccessResponse(res)
-    .setMessage('product has been updated successfully')
-    .send();
-});
+const updateProduct = catchAsyncRouteHandler<ProductSchema>(
+  async (req, res) => {
+    const { id } = req.params;
+
+    const updateDocument = req.sanitizeMongo.body.get([
+      'price',
+      'name',
+      'description',
+    ]);
+
+    const { matchedCount, modifiedCount } = await productModel.updateById(id, {
+      _useFieldsFromSchema: updateDocument,
+    });
+
+    if (matchedCount === 0) {
+      new ApiFailResponse(res).setMessage('product not found').notFound();
+      return;
+    }
+
+    const updateMessage = 'product has been updated successfully';
+    const notUpdateMessage = 'product found but nothing has updated';
+
+    new ApiSuccessResponse(res)
+      .setMessage(modifiedCount > 0 ? updateMessage : notUpdateMessage)
+      .send();
+  }
+);
 
 export default updateProduct;
