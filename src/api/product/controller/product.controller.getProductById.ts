4c36c178366088b9bb024169f2d989e03d7b9369
Muhammad Mustafa <mmi197here@gmail.com>
2023-08-28 05:30:04 +0300

    feat: handle 404 res in getProductById

--- src/api/product/controller/product.controller.getProductById.ts
+++ src/api/product/controller/product.controller.getProductById.ts
@@ -1,10 +1,19 @@
-import { ApiSuccessResponse, catchAsyncRouteHandler } from '../../../utils';
+import {
+  ApiFailResponse,
+  ApiSuccessResponse,
+  catchAsyncRouteHandler,
+} from '../../../utils';
 import productModel from '../product.model';
 
 const getProductById = catchAsyncRouteHandler(async (req, res) => {
   const productId = req.params.id;
 
   const product = await productModel.findById(productId);
+
+  if (product === null) {
+    new ApiFailResponse(res).setMessage("product hasn't been found").notFound();
+    return;
+  }
 
   ApiSuccessResponse.send(res, product);
 });
