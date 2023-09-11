63158f0de00181885ff9ed7fddf85ae5d01375d3
Muhammad Mustafa <mmi197here@gmail.com>
2023-09-12 02:22:44 +0300

    feat: add http logging for development

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,15 @@ import express, { Express } from 'express';
 import { MongoDb } from './utils';
 import apiRouter from './api';
 import expressErrorHandler from './middlewares/expressErrorHandler';
+import { envVars } from './config';
 
 const app: Express = express();
+
+// NOTE: using require because I don't want this code to be injected in production environment
+if (envVars.NODE_ENV !== 'production') {
+  // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
+  app.use(require('./middlewares/devHttpLogger').default);
+}
 
 app.use(express.json());
 app.use(MongoDb.sanitize);
